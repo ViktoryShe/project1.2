4f974f3ed10303fcb3a4a2eddce3f989ce754550
ViktiryShe <vika278@mail.ru>
2023-11-25 21:12:51 +0300

    Amended 1

--- js.js
+++ js.js
@@ -1,48 +1,25 @@
 // swiper
 
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
     pagination: {
-        el: '.swiper-pagination',
+      el: ".swiper-pagination",
+      clickable: true,
     },
-
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-});
+  });
 
 // скрыть-показать все
-const hideIcon = document.querySelector('.get-started__arrow');
 
 document.querySelector('.get-started__showMore').addEventListener('click', function() {
-    var lenovoButton = document.querySelector('.get-started__lenovo1');
-    if (lenovoButton.style.display === 'none') {
-        lenovoButton.style.display = 'block';
-      this.textContent = 'Скрыть';
-      hideIcon.style = 'transform: rotate(0deg)'
-    } else {
-        lenovoButton.style.display = 'none';
-      this.textContent = 'Показать все';
-      hideIcon.style = 'transform: rotate(180deg)'
-    }
-    var samsungButton = document.querySelector('.get-started__samsung1');
-    if (samsungButton.style.display === 'none') {
-        samsungButton.style.display = 'block';
-    } else {
-        samsungButton.style.display = 'none';
-    }
-    var appleButton = document.querySelector('.get-started__apple1');
-    if (appleButton.style.display === 'none') {
-        appleButton.style.display = 'block';
-    } else {
-        appleButton.style.display = 'none';
-    }
-  });
+const container = document.querySelector('.get-started__button');
+  if (container.style.height === '200px') {
+    container.style.height = '';
+    this.textContent = 'Скрыть';
+  } else {
+    container.style.height = '200px';
+    this.textContent = 'Показать все';
+  }
+  this.classList.toggle('rotate');
+});
 
