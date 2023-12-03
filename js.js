bca75dc78b4a6c6fff0d3713a5d1530c167c4e41
ViktiryShe <vika278@mail.ru>
2023-12-03 18:54:52 +0300

    Amended 3

--- js.js
+++ js.js
@@ -1,25 +1,33 @@
 // swiper
+new Swiper('.swiper',{
+  slidesPerView: "auto",
+  spaceBetween: 0,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 12,
+      autoplay:false,
+    }
+  }
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+});
 
 // скрыть-показать все
-const container = document.querySelector('.get-started__button');
 
-document.querySelector('.get-started__showMore').addEventListener('click', function() {
-  if (container.style.height === '200px') {
-    container.style.height = '';
+const container = document.querySelector('.swiper');
+const showMoreButton = document.querySelector('.get-started__showMore');
+
+showMoreButton.addEventListener('click', function() {
+  if (container.classList.contains('expanded')) {
+    container.classList.remove('expanded');
     this.textContent = 'Скрыть';
   } else {
-    container.style.height = '200px';
+    container.classList.add('expanded');
     this.textContent = 'Показать все';
   }
   this.classList.toggle('rotate');
 });
-
